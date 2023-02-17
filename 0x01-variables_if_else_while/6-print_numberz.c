1b65871bf31df5d8dc96e83ab07f00cd50f6d251
Frida <amina.amer.9898@gmail.com>
2023-02-17 15:13:55 -0800

    G

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ * description: this program will
+ * Return: return always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
