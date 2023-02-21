f9a710432ebba50cb35577c8656a974012b8dd27
Frida <amina.amer.9898@gmail.com>
2023-02-21 14:43:26 -0800

    G

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * print_alphabet_x10 - function will print alphabet ten times
+ *
+ * Return: always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int n, k;
+	
+	k = 0;
+
+	while (k < 10)
+	{
+	
+		for(n = 'a'; n<= 'z'; n++)
+		{
+		
+			_putchar(n);
+		}
+		k++;
+
+		_putchar('\n'); 
+	}
+}
