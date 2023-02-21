879382bceab40d7ebd1e1ba6591a78b30d0daaeb
Frida <amina.amer.9898@gmail.com>
2023-02-21 15:53:48 -0800

    G

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,12 +9,12 @@ void times_table(void)
 {
 	int rone, cone, d;
 
-	for (rone = 0; rone <= 10; rone++)
+	for (rone = 0; rone <= 9; rone++)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		for (cone = 1; cone <= 10; cone++)
+		for (cone = 1; cone <= 9; cone++)
 		{
 			d = (rone * cone);
 			if ((d / 10) > 0)
@@ -27,7 +27,7 @@ void times_table(void)
 			}
 			_putchar((d % 10) + '0');
 
-			if (cone < 10)
+			if (cone < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
