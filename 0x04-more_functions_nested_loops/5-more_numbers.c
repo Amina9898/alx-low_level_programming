ee39d8b179df9e5d77038625539e6165609487ad
Frida <amina.amer.9898@gmail.com>
2023-02-23 11:00:29 -0800

    G

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9, excluding 2 and 4, followed by new line
+ */
+
+void more_numbers(void)
+{
+	int j;
+
+	j = 0;
+
+	while (j < 10)
+	{
+		int i;
+
+		for (i = 0; i <= 9; i++)
+		{
+			_putchar(i + '0');
+		}
+			_putchar('\n');
+		j++;
+	}
+	_putchar('\n');
+}
