d83965058bfa8cc44b9499c0405002592ec6915f
Frida <amina.amer.9898@gmail.com>
2023-02-27 11:50:00 -0800

    G

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function is pissing me off
+ * @str : input
+ * Return: every other character of a string
+ */
+
+puts2(char *str)
+{
+	int i;
+
+	for (i = 0; ste[]i != '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
+	}
+	_putchar('\n');
+}
