3a5d8445a4d7bbb5edb297462633f5296aded4fd
Frida <amina.amer.9898@gmail.com>
2023-02-27 10:35:32 -0800

    G

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps
+ * @a : input
+ * @b: input
+ * return: 0
+ */
+
+void swap_int(int *a, int *b)
+{	
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
