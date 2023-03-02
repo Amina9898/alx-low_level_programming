482f4bc49f2de7ed516716099af49ea24f10f159
Frida <amina.amer.9898@gmail.com>
2023-03-01 16:32:06 -0800

    G

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * strncat - function will concatnate 2 strings
+ * @dest: first string
+ * @src: second string
+ * @n: number of bytes
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++);
+
+	for (n = 0; j < n; j++)
+	{
+		dest[i + j] = src[j];
+		if (src[j] == '\0')
+			j = n;
+	}
+		
+}
