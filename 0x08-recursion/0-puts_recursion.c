700666a53b70aaeb1e9632dad9b65d9f4794a275
Frida <amina.amer.9898@gmail.com>
2023-03-08 10:29:33 -0800

    G

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _puts_recursion - function that prints a string 
- * @s: pointer to a string 
- * Return: string 
+ * _puts_recursion - function that prints a string
+ * @s: pointer to a string
+ * Return: string
  */
 
 void _puts_recursion(char *s)
 {
 	if (*s)
-	{	
+	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
