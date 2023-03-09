8a5a2230e434847206786792f5b9e1c9bb91ae0c
Frida <amina.amer.9898@gmail.com>
2023-03-09 03:19:31 -0800

    G

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,30 @@
 #include "main.h"
 
+int squareroot(int n, int i);
 /**
- * _sqrt_recursion - dunction will return square root
- * @n : integer
- * Return: integer
+ * _sqrt_recursion - no loop, 1. 2nd function - checks for perfect square
+ * @n: input
+ * Return: Always 0 (Success)
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 0)
-		return (0);
-	else if (n == 1)
-		return (1);
-	else if (n < 0)
-		return (-1);
 	else
-		return (n / _sqrt_recursion(n));
+		return (squareroot(n, (n + 1) / 2));
+}
+/**
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+int squareroot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
 }
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,8 @@ int _putchar(char c);
 
 int factorial(int n);
 
+int squareroot(int n, int i);
+
 int _pow_recursion(int x, int y);
 
 int _sqrt_recursion(int n);
