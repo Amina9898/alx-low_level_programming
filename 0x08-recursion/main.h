3b5e30415ac28b7e2f8930a6c4e2457f6702e301
Frida <amina.amer.9898@gmail.com>
2023-03-08 17:26:07 -0800

    G

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 
-int _putchar (char c);
+int _putchar(char c);
 
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
-	
+
+int _strlen_recursion(char *s);
+
 #endif 
