39fb2322ae29be7e752e156d528832567d30b5b6
Frida <amina.amer.9898@gmail.com>
2023-03-08 18:27:27 -0800

    G

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - function will calculate power of x
+ * @x: input
+ * @y: integer input
+ * Return: intger
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
