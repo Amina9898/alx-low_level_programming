c24a63f36a6091566c44cd0cb103cf553a503392
Frida <amina.amer.9898@gmail.com>
2023-03-08 18:48:22 -0800

    G

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - dunction will return square root
+ * @n : integer
+ * Return: integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	else
+		return (n / _sqrt_recursion(n));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,6 +9,8 @@ int factorial(int n);
 
 int _pow_recursion(int x, int y);
 
+int _sqrt_recursion(int n);
+
 void _puts_recursion(char *s);
 
 void _print_rev_recursion(char *s);
