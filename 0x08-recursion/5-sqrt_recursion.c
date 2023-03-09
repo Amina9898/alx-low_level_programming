fb5212f1bb1ab24b7cd8789cd4b72372d0cc507e
Frida <amina.amer.9898@gmail.com>
2023-03-08 18:57:06 -0800

    G

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,10 +10,21 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n == 1)
-		return (1);
-	else if (n / _sqrt_recursion(n) != _sqrt_recursion(n))
-		return (n);
 	else
-		return (n / _sqrt_recursion(n));
+		return (squareroot(n, (n + 1) / 2));
+}
+/**
+ * squareroot - checks if perfect square
+ * @n: input
+ * @i: counter
+ * Return: if square root
+ */
+int squareroot(int n, int i)
+{
+	if (i < 1)
+		return (-1);
+	else if (i * i == n)
+		return (i);
+	else
+		return (squareroot(n, i - 1));
 }
