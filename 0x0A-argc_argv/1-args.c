dddc74a5e5bdca1cc5b0193733c84429089b121d
Frida <amina.amer.9898@gmail.com>
2023-03-10 16:44:41 -0800

    G

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -12,9 +12,9 @@ int main(int argc, __attribute__((unused))char *argv[])
 {
 	int i;
 
-	for (i = 1; i < argc; i++)
+	for (i = 0; i < argc; i++)
 	{
 	}
-	printf("%d\n", i);
+	printf("%d\n", i - 1);
 	exit(EXIT_SUCCESS);
 }
