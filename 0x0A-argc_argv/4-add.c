edd11b07a4e6677b08ffb9d122afd212c5754b0c
Frida <amina.amer.9898@gmail.com>
2023-03-10 18:23:40 -0800

    G

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function will add positive numbers
+ * @argc: count of arguments
+ * @argv: pointers to arguments
+ * Return: addition if positive and 1 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+	int sum = 0;
+
+	if (argc < 1)
+		return (0);
+		
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	exit(EXIT_SUCCESS);
+}
