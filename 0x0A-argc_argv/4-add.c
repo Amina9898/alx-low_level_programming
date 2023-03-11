5235e9c8f92c4dc9adb9d13083a8ef5ca09071ab
Frida <amina.amer.9898@gmail.com>
2023-03-10 18:26:48 -0800

    G

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		return (0);
 	for (i = 1; i < argc; i++)
 	{
-		if (!atoi(argv[i]))
+		if ((only_nums(*argv) == 0))
 		{
 			printf("%s\n", "Error");
 			return (1);
