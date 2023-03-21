0a5e25fe1b61fe6c18ba9ed89e4ff6e2d4b0ecbe
Frida <amina.amer.9898@gmail.com>
2023-03-20 18:47:50 -0700

    G

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - short description
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG_H */
