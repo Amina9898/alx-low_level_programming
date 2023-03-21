43916d72a2805e2b64d149134ee16dc35aa9ed46
Frida <amina.amer.9898@gmail.com>
2023-03-20 19:22:10 -0700

    G

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,22 +2,21 @@
 #define DOG_H
 
 /**
- * struct dog - short description
- * @name: char
- * @age: float
- * @owner: char
- *
- * Description: longer description
+ * struct dog - structure
+ * @name: name
+ * @age: age
+ * @owner: owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
-typedef struct dog dog_t;
-void free_dog(dog_t *d);
-dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /* DOG_H */
