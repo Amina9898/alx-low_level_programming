41c6485cb083c6009be0dd33f8ad4f3429bf079e
Frida <amina.amer.9898@gmail.com>
2023-03-30 16:36:00 -0700

    G

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h> 
+#include <stddef.h>
+
+/**
+ * list_len - dunction will return number of elements in a list
+ * @h: pointer to list
+ * Return: number of elements
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t elements = 0;
+
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
+
+	return (elements);
+}
