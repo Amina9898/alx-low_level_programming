ca8aab7bdb632578955831934c4bd15308c27938
Frida <amina.amer.9898@gmail.com>
2023-04-04 12:52:43 -0700

    G

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -11,11 +11,13 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int count;
+	unsigned int count = 0;
 
-	while (head != NULL)
+	if (!head)
+		return (0);
+
+	while (head)
 	{
-		count = 0;
 		if (count == index)
 			return (head);
 		count++;
