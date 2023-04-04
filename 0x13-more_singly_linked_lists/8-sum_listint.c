441bff9ba67a38d49b30bcc14ee58c299e79bcaa
Frida <amina.amer.9898@gmail.com>
2023-04-04 13:26:51 -0700

    G

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function sums all data (n) of a
+ * listint_t linked list
+ * @head: pointer to list
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (!head)
+		return (0);
+	
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}
