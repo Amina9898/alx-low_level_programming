82bdbb2bebfc2d9b86c486b4996d943106822835
Frida <amina.amer.9898@gmail.com>
2023-04-04 13:28:45 -0700

    G

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,6 +4,7 @@
  * sum_listint - function sums all data (n) of a
  * listint_t linked list
  * @head: pointer to list
+ * Return: sum
  */
 
 int sum_listint(listint_t *head)
@@ -12,7 +13,7 @@ int sum_listint(listint_t *head)
 
 	if (!head)
 		return (0);
-	
+
 	while (head)
 	{
 		sum = sum + head->n;
