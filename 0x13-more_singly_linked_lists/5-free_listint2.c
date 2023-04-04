77af9852b365cdd1219980e63608232dd081a0ef
Frida <amina.amer.9898@gmail.com>
2023-04-04 02:47:23 -0700

    G

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,9 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *p;
-	
-	while(*head != NULL)
-	{
 
+	while(*head != NULL || head != NULL)
+	{
 		p = *head;
 		*head = (*head)->next;
 		free(p);
