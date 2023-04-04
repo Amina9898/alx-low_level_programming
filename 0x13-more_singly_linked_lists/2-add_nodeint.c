94f15aab4672dadecf3fae862bc1409b5a259ba4
Frida <amina.amer.9898@gmail.com>
2023-04-03 20:07:27 -0700

    G

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,11 +12,14 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *p = malloc(sizeof(listint_t));
+
 	p->n = n;
 	p->next = NULL;
 
 	p->next = *head;
 	*head = p;
-		
+
+	if (!p)
+		return (NULL);
 	return (p);
 }
