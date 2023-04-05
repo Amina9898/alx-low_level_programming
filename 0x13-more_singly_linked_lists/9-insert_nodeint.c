0687454c54312dc249759d9fdbb35406a4938c33
Frida <amina.amer.9898@gmail.com>
2023-04-04 21:50:17 -0700

    G

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (!head || !*head || !p)
 		return (NULL);
-	while (p)
+	while (p != NULL)
 	{
 		if (idx == count)
 			break;
