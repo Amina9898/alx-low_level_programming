27ef166290cb8a6405ef940c667dbc877b09dc2f
Frida <amina.amer.9898@gmail.com>
2023-04-09 15:27:52 -0700

    G

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,15 +6,16 @@
  * one number to another.
  * @n: first input number
  * @m: second input number
- * Retrun: returns the number of bits you would need to flip
+ * Return: returns the number of bits you would need to flip
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int b, cross;
-	cross = n ^ m;
-	b = 0;
 
+	cross = n ^ m;
+
+	b = 0;
 	while (cross > 0)
 	{
 		b += (cross);
