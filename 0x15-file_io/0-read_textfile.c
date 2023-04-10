80182b3598875b1ee2c26a046eb64a7c040e3aff
Frida <amina.amer.9898@gmail.com>
2023-04-10 16:03:07 -0700

    G

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * it to the POSIX standard output
  * @filename: pointer to an array of chars
  * @letters: the number of letters it should read and print
- * Return: if file cannot be opened, filename = 0, 
+ * Return: if file cannot be opened, filename = 0,
  * if write fails , return 0. Or else return
  * the actual number of letters it could
  * read and print
@@ -20,17 +20,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
+
 	if (buff == NULL)
 		return (0);
 
 	of = open(filename, O_RDONLY);
 	rf = read(of, buff, letters);
 	wf = write(STDOUT_FILENO, buff, rf);
-	
+
 	if (of == -1 || rf == -1 || wf == -1 || rf != wf)
 	{
 		free(buff);
-		return(0);
+		return (0);
 	}
 
 	free(buff);
