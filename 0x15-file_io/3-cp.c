4d63f1d3b7685c3eca6ff24f020f07c869b120b1
Frida <amina.amer.9898@gmail.com>
2023-04-11 13:54:33 -0700

    G

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,13 +13,45 @@ char *make_buff(char *f)
 	char *buff;
 
 	buff = malloc(sizeof(char) * 1024);
+
+	if (buff == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
+		exit(99);
+	}
 	
 	return (buff);
 }
 
+/**
+ * close_file - function will close files
+ * @fd: the file descriptor
+ */
+
+void close_file(int fd)
+{
+	int cf;
+
+	cf = close(fd);
+
+	if (cf == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+ 
+/**
+ * main - program that copies from f1 to f2
+ * @argc: number of args
+ * @argv: an array of pointers to args
+ * Return: 0 if successful
+ *
+ * Description: if argc is not correct, exit code 97
+ */
 int main(int argc, char *argv[])
 {
-	int f1, f2, rf;
+	int f1, f2, rf, wf;
 	char *buff;
 	if (argc != 3)
 	{
@@ -34,16 +66,29 @@ int main(int argc, char *argv[])
 
 	do
 	{
-		int wf;
+		if (f1 == -1 || rf == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(99);
+		}
 
 		wf = write(f2, buff, rf);
-		rf = read(f1, buff, 1024);
-		f2 = open(arg[2], O_WRONLY | O_APPEND);
-	} while (rf > 0);
 
-	free(buff);
-	close(f1);
-	close(f2);
+		if (f2 == -1 || wf == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buff);
+			exit(99);
+		}
+
+		rf = read(f1, buff, 1024);
+		f2 = open(argv[2], O_WRONLY | O_APPEND);
 	
+	} while (rf > 0);
+	
+	free(buff);
+	close_file(f1);
+	close_file(f2);
+
 	return (0);
 }
