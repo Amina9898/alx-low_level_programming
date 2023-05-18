27e9ac50f500eec4adf72026254acb3a768103fe
Frida <amina.amer.9898@gmail.com>
2023-05-18 16:45:20 -0700

    Betty fix

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new;
 
 	new = malloc(sizeof(dlistint_t));
-	
+
 	if (!new)
 		return (NULL);
 
@@ -24,7 +24,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (*head != NULL)
 		(*head)->prev = new;
-	
+
 	*head = new;
 
 	return (new);
