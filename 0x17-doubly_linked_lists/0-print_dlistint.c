cacb46b218b09ac49cd8b821a5c09c61d4dbe7d0
Frida <amina.amer.9898@gmail.com>
2023-05-18 15:29:57 -0700

    Task 0 and header file

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_dlinsint - function that prints all the elements of a
+ * dlistint_t list.
+ * @h: head of list
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *ptr;
+	size_t i;
+	ptr = h;
+	i = 0;
+
+	while(ptr != NULL)
+	{
+		printf("%d\n", ptr->n);
+		ptr = ptr->next;
+		i++;
+	}
+	return(i);
+}
