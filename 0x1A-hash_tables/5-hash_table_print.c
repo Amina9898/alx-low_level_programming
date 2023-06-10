0218532c960502d2cf6e41f8c3b33756cd0c0b92
Frida <amina.amer.9898@gmail.com>
2023-06-10 01:52:03 -0700

    Format fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -15,12 +15,9 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned int count = 0;
 
 	if (ht == NULL)
-	{
 		return;
-	}
 
 	printf("{");
-
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
@@ -28,7 +25,7 @@ void hash_table_print(const hash_table_t *ht)
 			if (count == 1)
 				printf(", ");
 
-			node = ht->array[i];
+		node = ht->array[i];
 
 		while (node != NULL)
 		{
