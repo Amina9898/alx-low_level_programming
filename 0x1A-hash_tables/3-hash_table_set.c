fa0486eb6014465855ff395dddcb02fd19a80e98
Frida <amina.amer.9898@gmail.com>
2023-06-10 02:54:13 -0700

    Output errors fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -30,21 +30,36 @@ hash_node_t *create_node(const char *key, const char *value)
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int hash;
-	int index;
+	unsigned long int index, i;
+	char *val;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	hash = hash_djb2((unsigned char *)key);
-	index = hash % ht->size;
+	val = strdup(value);
+	if (val == NULL)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = val;
+			return (1);
+		}
+	}
+
 
 	new_node = create_node(key, value);
 
 	if (new_node == NULL)
+	{
+		free(new_node);
 		return (0);
-
+	}
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
